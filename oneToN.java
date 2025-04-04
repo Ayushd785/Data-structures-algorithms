d5757688047c5afcf6560d517aacc9b92b1bb059
Ayushd785 <ayushd785@gmail.com>
2025-04-04 17:19:23 +0530

    oneToN done

---
+++ oneToN.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class oneToN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print(n);
+
+    }
+    static void print(int n){
+        if(n ==0){
+            return;
+        }
+        print(n-1);
+        System.out.println(n);
+        
+    }
+}
