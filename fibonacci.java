426115202f1c8a790e218e37f4a3b465f13c2bf7
Ayushd785 <ayushd785@gmail.com>
2025-04-04 17:15:25 +0530

    fibonacci

---
+++ fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        int n = sc.nextInt();
+        int ans =fib(n);
+        System.out.println(ans);
+
+
+    }
+    public static int fib(int n){
+        if(n<2){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
